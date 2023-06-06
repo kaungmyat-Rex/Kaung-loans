d075bf475c3202eed0c023b96555452de9d7078c
KaungMyat <aungakm667@gmail.com>
2023-06-06 14:00:44 +0630

    fix manifest deploy error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-app/",
+
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
