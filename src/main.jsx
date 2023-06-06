c04a58a39fe03556ba24b5354daf37a163d4e74d
KaungMyat <aungakm667@gmail.com>
2023-06-06 13:52:54 +0630

    fix manifest.json files

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import Store from "./redux/Store.js";
 import { Provider } from "react-redux";
-import "./public/manifest.json";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
